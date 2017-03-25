bca3e40796995847b2e5b711d9bdd56a8cbce0cc
Stuart Wheaton <swheaton5@gmail.com>
2017-03-25 13:59:56 -0400

    Initial commit. NXT HelloWorld

---
+++ src/HelloWorld.java
@@ -0,0 +1,13 @@
+
+import lejos.nxt.Button;
+import lejos.nxt.Motor;
+
+public class HelloWorld {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Button.waitForAnyPress();
+    }
+}

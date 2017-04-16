edbb18656d2fede9b55b57019f2a9a18a3271d6c
Stuart Wheaton <swheaton5@gmail.com>
2017-04-16 19:19:40 -0400

    listen to escape button to quit program

--- src/stuartw_at_umich_dot_edu/MindstormRobot/Robot.java
+++ src/stuartw_at_umich_dot_edu/MindstormRobot/Robot.java
@@ -1,11 +1,24 @@
 package stuartw_at_umich_dot_edu.MindstormRobot;
 
+import lejos.nxt.Button;
+import lejos.nxt.ButtonListener;
 import lejos.robotics.subsumption.Arbitrator;
 import lejos.robotics.subsumption.Behavior;
 
 public class Robot {
 	public static void main(String[] args)
 	{
+		// Bind the enter button to escape the program
+		Button.ESCAPE.addButtonListener(new ButtonListener() {
+		   public void buttonPressed(Button b) {
+		      System.exit(0);
+		   }
+
+		   public void buttonReleased(Button b) {
+		      // Nothing here
+		   }
+		});
+
 		MoveBehavior moveBehav = new MoveBehavior();
 		Behavior [] behavArray = {moveBehav};
 		Arbitrator arby = new Arbitrator(behavArray);
